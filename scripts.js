bd626de88f9e244b30a276ead0d1ca7dc74302ff
okrobo <kevinokrobo@gmail.com>
2025-03-07 17:08:41 -0500

    Update scripts.js1

--- scripts.js
+++ scripts.js
@@ -28,3 +28,28 @@ function toggleSidebar(sidebarId) {
     var sidebar = document.getElementById(sidebarId);
     sidebar.classList.toggle('expanded'); // Toggle the 'expanded' class to show/hide content
 }
+
+document.getElementById('bold-btn').addEventListener('click', function() {
+    document.execCommand('bold');
+});
+
+document.getElementById('italic-btn').addEventListener('click', function() {
+    document.execCommand('italic');
+});
+
+document.getElementById('strike-btn').addEventListener('click', function() {
+    document.execCommand('strikeThrough');
+});
+
+document.getElementById('bullet-btn').addEventListener('click', function() {
+    document.execCommand('insertUnorderedList');
+});
+
+document.getElementById('underline-btn').addEventListener('click', function() {
+    document.execCommand('underline');
+});
+
+document.getElementById('link-btn').addEventListener('click', function() {
+    let url = prompt("Enter the link URL:");
+    document.execCommand('createLink', false, url);
+});
